b6478f3019e0340f3008649efe561ec03f7c5648
MLNotes2718 <150231238+mlnotes2718@users.noreply.github.com>
2025-05-13 16:53:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ palm.configure(api_key=api_key)
 
 #defaults = { 'model': "models/text-bison-001"}
 app = Flask(__name__)
-model = palm.GenerativeModel("gemini-1.5-flash")
+model = palm.GenerativeModel("models/text-bison-001")
 
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
         t = request.form.get("txt")
-        #r = palm.generate_text(**defaults,messages=t)
-        r = model.generate_content(t)
-        #return(render_template("index.html",result=r.last))
-        return(render_template("index.html",result=r.candidates[0].content.parts[0].text))
+        r = model.generate_text(t)
+        #r = model.generate_content(t)
+        return(render_template("index.html",result=r.last))
+        #return(render_template("index.html",result=r.candidates[0].content.parts[0].text))
     else:
         return(render_template("index.html",result="waiting"))
 
