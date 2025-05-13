07da48dbcf2a20e73654b3118ad87e21425cd364
MLNotes2718 <150231238+mlnotes2718@users.noreply.github.com>
2025-05-13 07:01:36 +0000

    add files

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import google.generativeai as palm
+import os
+
+api_key = os.getenv("GOOGLE_API")
+palm.configure(api_key=api_key)
+
+defaults = { 'model': "models/text-bison-001"}
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        t = request.form.get("txt")
+        r = palm.generate_test(**defaults,messages=t)
+        return(render_template("index.html",result=r.last))
+    else:
+        return(render_template("index.html",result="waiting"))
+
+if __name__ == "__main__":
+    app.run()
