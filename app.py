37ed769e478fe8537e24a78870c72c4b8ad208ac
MLNotes2718 <150231238+mlnotes2718@users.noreply.github.com>
2025-05-13 16:48:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ import os
 api_key = os.getenv("GOOGLE_API")
 palm.configure(api_key=api_key)
 
-defaults = { 'model': "models/text-bison-001"}
+#defaults = { 'model': "models/text-bison-001"}
 app = Flask(__name__)
+model = palm.GenerativeModel("gemini-1.5-flash")
 
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
         t = request.form.get("txt")
-        r = palm.generate_text(**defaults,messages=t)
-        return(render_template("index.html",result=r.last))
+        #r = palm.generate_text(**defaults,messages=t)
+        r = model.generate_content(t)
+        #return(render_template("index.html",result=r.last))
+        return(render_template("index.html",result=r.candidates[0].content.parts[0].text))
     else:
         return(render_template("index.html",result="waiting"))
 
